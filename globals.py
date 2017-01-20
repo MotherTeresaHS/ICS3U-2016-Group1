5f0ecc848d61cf43fbc03d3536b4bd2ea880b200
KyleBoulay <Kyle.boulay@ocsbstudent.ca>
2017-01-20 13:04:24 -0500

    Create globals.py

---
+++ globals.py
@@ -0,0 +1,40 @@
+class Globals():
+    #stats
+    global fullhealth
+    fullhealth = 100
+    global playerdmglowest
+    playerdmglowest = 20
+    global playerdmghighest
+    playerdmghighest = 50
+    global playercritchance
+    playercritchance = 10
+    global playercritdmg
+    playercritdmg = 150
+    global overtimeregen
+    overtimeregen = 5
+    global playerarmor
+    playerarmor = 5
+    global playeratkspeed
+    playeratkspeed = 1
+    
+    #coins
+    global coins
+    coins = 0
+    
+    #charaterstate
+    global currentstate
+    currentstate = 'run'
+    
+    #prices
+    global price1
+    price1 = 100
+    global price2
+    price2 = 100
+    global price3
+    price3 = 100
+    global price4
+    price4 = 100
+    global price5
+    price5 = 100
+    global price6
+    price6 = 100
